ddc4bfd1b822d1dea5c6c31ecf08652fe77a94e5
Dmitry <dvoskoboev@gmail.com>
2021-02-26 17:54:26 +0300

    replaced px with rem for typography, added responsive to tablets' size

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const initSwiper = () => {
   var mySwiper = new Swiper('.photo__carousel', {
     direction: 'horizontal',
+    centeredSlides: true,
     loop: true,
     navigation: {
       nextEl: '.swiper-button-next',
